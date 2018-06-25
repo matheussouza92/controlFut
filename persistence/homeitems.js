05eed1163b2149c6826de9a4e0fd87a0f88b3048
Matheus Souza Santana <msantana.unb@gmail.com>
2018-06-24 21:38:16 -0300

    Sub topo Header2.0

--- persistence/homeitems.js
+++ persistence/homeitems.js
@@ -1,15 +1,18 @@
 'use strict';
 
 export const HEADER_ITEMS = {
-    CONVIDADO:      'CONVIDADO',
-    SORTEIO:        'SORTEIO',
-    EDITAR:         'EDITAR',
-    COMPARTILHAR:   'COMPARTILHAR'
-}
+    CONVIDADO: 'CONVIDADO',
+    SORTEIO: 'SORTEIO',
+    BLANK: 'BLANK',
+    EDITAR: 'EDITAR',
+    COMPARTILHAR: 'COMPARTILHAR'
+};
 
 export const ITEMS = [
-    {ordination: 1, desc: 'convidado',      identifier: HEADER_ITEMS.CONVIDADO},
-    {ordination: 2, desc: 'sorteio',        identifier: HEADER_ITEMS.SORTEIO},
-    {ordination: 3, desc: 'editar',         identifier: HEADER_ITEMS.EDITAR},
-    {ordination: 4, desc: 'compartilhar',   identifier: HEADER_ITEMS.COMPARTILHAR}
+    { ordination: 1, desc: 'convidado', identifier: HEADER_ITEMS.CONVIDADO, icon: 'user-plus' },
+    { ordination: 2, desc: 'sorteio', identifier: HEADER_ITEMS.SORTEIO, icon: 'random' },
+    { ordination: 3, desc: '', identifier: HEADER_ITEMS.BLANK, icon: '' },
+    { ordination: 4, desc: '', identifier: HEADER_ITEMS.BLANK, icon: '' },
+    { ordination: 5, desc: 'editar', identifier: HEADER_ITEMS.EDITAR, icon: 'edit' },
+    { ordination: 6, desc: 'compartilhar', identifier: HEADER_ITEMS.COMPARTILHAR, icon: 'share-square' }
 ];
