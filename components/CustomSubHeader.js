845e617a99af6ecd7165078d95b0f570f0cc7481
Unknown <tarcisiodf23@gmail.com>
2018-06-24 22:07:13 -0300

    Retirando a porra do ItensTop de novo e arrumando centralização.

--- components/CustomSubHeader.js
+++ components/CustomSubHeader.js
@@ -4,27 +4,27 @@ import {
     StyleSheet,
     Text
 } from 'react-native';
-import {HEADER_ITEMS, ITEMS} from '../persistence/homeitems';
+import { HEADER_ITEMS, ITEMS } from '../persistence/homeitems';
 import { Icon } from 'react-native-elements';
 
 //const WIDTH_SCREEN = Dimensions.get('screen').width;
 
 export default class CustomSubHeader extends Component {
 
-    renderTopItens ()  {
+    renderTopItens() {
         return (
             ITEMS.map((item) => {
                 return (
-                    <View style = {styles.item} key = {item.ordination}>
-                        <Icon 
-                            style = {styles.icon}
+                    <View style={styles.item} key={item.ordination}>
+                        <Icon
+                            style={styles.icon}
                             raised
-                            name= {item.icon}
-                            size = {13}
+                            name={item.icon}
+                            size={13}
                             type='font-awesome'
-                            onPress={() => console.log('hello')} 
+                            onPress={() => console.log('hello')}
                         />
-                        <Text style = {styles.itemText}>{item.desc}</Text>
+                        <Text style={styles.itemText}>{item.desc}</Text>
                     </View>
                 );
             })
@@ -33,9 +33,9 @@ export default class CustomSubHeader extends Component {
 
     render() {
         return (
-            <View style = {styles.container}>
-                TopItens = {this.renderTopItens()}
-            </View> 
+            <View style={styles.container}>
+                {this.renderTopItens()}
+            </View>
         );
     }
 }
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         marginTop: -1,
-        height: 60, 
+        height: 60,
         backgroundColor: '#C1E1FA',
         alignContent: 'center'
     },
@@ -52,14 +52,16 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         alignContent: 'center'
     },
-    item: {   
-        flex: 1,    
-        width:60,
-        height: 60, 
-        backgroundColor: '#F0F1F1'
+    item: {
+        flex: 1,
+        width: 60,
+        height: 60,
+        backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     itemText: {
-        color: '#000', 
+        color: '#000',
         textAlign: 'center',
         fontSize: 10
     }
